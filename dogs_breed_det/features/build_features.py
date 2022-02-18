9e667e9ce37bc061ab3c13d980d7053c047e7ecd
hpadhaln <hem.chandra@corestack.io>
2022-02-18 11:52:50 +0530

    initial commit

---
+++ dogs_breed_det/features/build_features.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Feature building
+"""
+
+# import project config.py
+import dogs_breed_det.config as cfg
---
+++ dogs_breed_det/visualization/visualize.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Visualization
+"""
+# import project config.py
+import dogs_breed_det.config as cfg
